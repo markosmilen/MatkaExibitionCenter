850c800f54a9dfa2638a2eb829fd255b7eb5c766
marko <markosmilen@gmail.com>
2019-12-01 22:45:47 +0100

    sixth commit: created: - created recyclerView in VideoFragment
    to do:
    - add listener to VideoFragment
    - add listener to History fragment
    - add Library to zoom images in History Fragment
    - fill other fragments

---
+++ app/src/main/java/com/example/matkaexibitioncenter/adapters/VideoAdapter.java
@@ -0,0 +1,58 @@
+package com.example.matkaexibitioncenter.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.matkaexibitioncenter.R;
+import com.example.matkaexibitioncenter.models.ExibitModel;
+
+import java.util.ArrayList;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHolder> {
+
+    Context context;
+    ArrayList<ExibitModel> dataVideos;
+    LayoutInflater inflater;
+
+    public VideoAdapter(Context context, ArrayList<ExibitModel> dataVideos) {
+        this.context = context;
+        this.dataVideos = dataVideos;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_video, parent, false);
+        return new VideoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
+        ExibitModel video1 = dataVideos.get(position);
+        holder.image.setImageResource(video1.getImage());
+        holder.title.setText(video1.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataVideos.size();
+    }
+
+    public class VideoViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView title;
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = (ImageView) itemView.findViewById(R.id.item_video_videoimg_id);
+            title = (TextView) itemView.findViewById(R.id.item_video_title_id);
+        }
+    }
+}
--- app/src/main/java/com/example/matkaexibitioncenter/fragments/VideoFragment.java
+++ app/src/main/java/com/example/matkaexibitioncenter/fragments/VideoFragment.java
@@ -1,5 +1,6 @@
 package com.example.matkaexibitioncenter.fragments;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,12 +12,22 @@ import android.widget.VideoView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.matkaexibitioncenter.R;
+import com.example.matkaexibitioncenter.adapters.VideoAdapter;
+import com.example.matkaexibitioncenter.models.ExibitModel;
+
+import java.util.ArrayList;
 
 public class VideoFragment extends Fragment {
     public static final String TAG = VideoFragment.class.getSimpleName();
     VideoView videoview;
+    RecyclerView recyclerView;
+    VideoAdapter adapter;
+    ArrayList<ExibitModel> videos = new ArrayList<>();
 
     public static VideoFragment newInstance(){
         VideoFragment videoFragment = new VideoFragment();
@@ -28,6 +39,12 @@ public class VideoFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_viceo, container, false);
         videoview = (VideoView) view.findViewById(R.id.videocover_videofragment_id);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview_videofragment_id);
+
+        getVideos();
+        adapter = new VideoAdapter(getContext(), videos);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2, GridLayoutManager.HORIZONTAL, false));
+        recyclerView.setAdapter(adapter);
 
         Uri uri = Uri.parse("android.resource://"+getActivity().getPackageName()+"/"+R.raw.videoplayback);
         videoview.setVideoURI(uri);
@@ -41,6 +58,63 @@ public class VideoFragment extends Fragment {
             }
         });
         return view;
+
+
+
+
+    }
+
+    public  void getVideos(){
+        ExibitModel video1 = new ExibitModel();
+        video1.setImage(R.drawable.video_img_1);
+        video1.setTitle("Денот на Тесла");
+        videos.add(video1);
+
+        ExibitModel video2 = new ExibitModel();
+        video2.setImage(R.drawable.video_img_2);
+        video2.setTitle("Цртежи – Денот на Тесла");
+        videos.add(video2);
+
+        ExibitModel video3 = new ExibitModel();
+        video3.setImage(R.drawable.video_img_3);
+        video3.setTitle("Историја на електрификација во Македонија");
+        videos.add(video3);
+
+        ExibitModel video4 = new ExibitModel();
+        video4.setImage(R.drawable.video_img_4);
+        video4.setTitle("Енергетска ефикасност");
+        videos.add(video4);
+
+        ExibitModel video5 = new ExibitModel();
+        video5.setImage(R.drawable.video_img_5);
+        video5.setTitle("Патот на струјата");
+        videos.add(video5);
+
+        ExibitModel video6 = new ExibitModel();
+        video6.setImage(R.drawable.video_img_6);
+        video6.setTitle("Како се пренесува струјата?");
+        videos.add(video6);
+
+        ExibitModel video7 = new ExibitModel();
+        video7.setImage(R.drawable.video_img_7);
+        video7.setTitle("Обновливи извори на енергија");
+        videos.add(video7);
+
+        ExibitModel video8 = new ExibitModel();
+        video8.setImage(R.drawable.video_img_8);
+        video8.setTitle("Што е струја?");
+        videos.add(video8);
+
+
+
+
+    }
+    public void openYoutube(View view) {
+        String videoId = "ui2-ca-Cr7o";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:"+videoId));
+        intent.putExtra("VIDEO_ID", videoId);
+        intent.putExtra("force_fullscreen",true);
+        startActivity(intent);
     }
 
 
